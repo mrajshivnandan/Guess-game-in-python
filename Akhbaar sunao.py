b3a836975e17cbef3421df63588b52135d964dc4
Raj Mishra <68726550+mrajshivnandan@users.noreply.github.com>
2020-08-15 15:11:43 +0530

    Listen updated news daily
    Use my python program to hear daily news easily and anytime you want. Feel free to use my code.

--- Akhbaar sunao.py
+++ Akhbaar sunao.py
@@ -5,7 +5,7 @@ import json
 def speak(str):
     speak= Dispatch('SAPI.SpVoice')
     speak.Speak(str)
-# 74bee1588dc24458a311140342832ccc
+
 di1 = {1: 'business', 2: 'entertainment', 3: 'general', 4: 'health', 5: 'science', 6: 'sports', 7: 'technology'}
 
 if __name__=='__main__':
